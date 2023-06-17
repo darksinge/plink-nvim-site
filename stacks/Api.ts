e2889db8b3b7cd4b16aa1ab41393917af8b894a9
Craig Blackburn <craig.blackburn@youscience.com>
2023-06-17 17:16:36 -0600

    add rate limiting settings

--- stacks/Api.ts
+++ stacks/Api.ts
@@ -6,6 +6,12 @@ export function API({ stack }: StackContext) {
       'GET /search': 'packages/functions/src/search.query',
       'GET /tags': 'packages/functions/src/tags.get',
     },
+    defaults: {
+      throttle: {
+        burst: 100,
+        rate: 10,
+      },
+    },
   })
 
   stack.addOutputs({
